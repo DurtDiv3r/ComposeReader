a120c994d916e314b11f9325e008df2835aebf5d
DurtDiv3r <75084231+DurtDiv3r@users.noreply.github.com>
2023-02-16 12:52:03 +1300

    Compose Reader: Add dependencies. Add FireStore and test add user.

--- app/src/main/java/com/example/composereader/MainActivity.kt
+++ app/src/main/java/com/example/composereader/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.composereader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,17 +12,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composereader.ui.theme.ComposeReaderTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeReaderTheme {
+                //Initial setup of FireStore... Add test user
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+
+                user["firstName"] = "Bob"
+                user["lastName"] = "Smith"
+
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    db.collection("users").add(user).addOnSuccessListener {
+                        Log.d("FireBase", "onCreate ${it.id}")
+                    }.addOnFailureListener {
+                        Log.d("FireBase", "Failed to add user ${it}")
+                    }
                     Greeting("Android")
                 }
             }
