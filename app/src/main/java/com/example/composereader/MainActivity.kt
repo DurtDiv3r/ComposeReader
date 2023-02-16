20a021b634e4c278e3f467adbd1157a2d5e4a693
DurtDiv3r <75084231+DurtDiv3r@users.noreply.github.com>
2023-02-16 15:52:51 +1300

    Compose Reader: Add navigation framework and possible screens.

--- app/src/main/java/com/example/composereader/MainActivity.kt
+++ app/src/main/java/com/example/composereader/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.example.composereader
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.composereader.navigation.AppNavigation
 import com.example.composereader.ui.theme.ComposeReaderTheme
-import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,39 +23,46 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeReaderTheme {
                 //Initial setup of FireStore... Add test user
-                val db = FirebaseFirestore.getInstance()
-                val user: MutableMap<String, Any> = HashMap()
+//                val db = FirebaseFirestore.getInstance()
+//                val user: MutableMap<String, Any> = HashMap()
+//
+//                user["firstName"] = "Bob"
+//                user["lastName"] = "Smith"
 
-                user["firstName"] = "Bob"
-                user["lastName"] = "Smith"
 
+                ComposeReaderApp()
 
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    db.collection("users").add(user).addOnSuccessListener {
-                        Log.d("FireBase", "onCreate ${it.id}")
-                    }.addOnFailureListener {
-                        Log.d("FireBase", "Failed to add user ${it}")
-                    }
-                    Greeting("Android")
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ComposeReaderApp() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+//                    db.collection("users").add(user).addOnSuccessListener {
+//                        Log.d("FireBase", "onCreate ${it.id}")
+//                    }.addOnFailureListener {
+//                        Log.d("FireBase", "Failed to add user ${it}")
+//                    }
+
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            AppNavigation()
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeReaderTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/composereader/screens/SplashScreen.kt
@@ -0,0 +1,48 @@
+package com.example.composereader.screens
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.navigation.NavController
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.composereader.navigation.AppScreens
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.9f,
+            animationSpec = tween(durationMillis = 1000, easing = {
+                OvershootInterpolator(8f).getInterpolation(it)
+            })
+        )
+        delay(2000)
+        navController.navigate(AppScreens.LoginScreen.name)
+    }
+
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .scale(scale = scale.value)
+    ) {
+
+        val composition by rememberLottieComposition(spec = LottieCompositionSpec.Url("https://assets2.lottiefiles.com/private_files/lf30_FJSIAJ.json"))
+        LottieAnimation(composition = composition, iterations = 1)
+    }
+}
